f823000f78d2e43a78c520c9b61741904bf2260f
NusfaJahan61 <nusfaikra@gmail.com>
2026-02-21 23:46:41 +0600

    Setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-green-700 text-2xl font-bold'>BrickBloom</h1>
+    </>
+  )
+}
+
+export default App
